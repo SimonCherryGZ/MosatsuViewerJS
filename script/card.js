ce12768d2821cd822b5a29a83204e99ed62dd379
SimonCherry <simoncherry@sina.com>
2017-06-28 22:01:05 +0800

    fix: 改变浏览器窗口大小后，Canvas描绘路径错乱。

--- script/card.js
+++ script/card.js
@@ -1,101 +1,120 @@
 /**
  * Created by Simon on 2017/6/27.
  */
+var canvas;
+var ctx;
+var imgTop;
+var imgBottom;
+var imgScale = 0.6;
+var mouseDown = false;
+var dstWidth;
+var dstHeight;
+var offsetX;
+var offsetY;
+
 window.onload = function () {
     var bodyStyle = document.body.style;
     bodyStyle.mozUserSelect = 'none';
     bodyStyle.webkitUserSelect = 'none';
 
-    initCanvas();
+    initImage();
 };
 
 window.addEventListener("resize", function () {
     initCanvas();
 });
 
-function initCanvas() {
-    var imgBottom = new Image();
-    var canvas = document.querySelector('canvas');
-    canvas.style.backgroundColor = 'transparent';
-    canvas.style.position = 'absolute';
-    imgBottom.src = '../images/sample1_a.jpg';
-    var imgScale = 0.6;
+function initImage() {
+    canvas = document.querySelector('canvas');
+
+    imgTop = new Image();
+    imgTop.src = '../images/sample1_a.jpg';
+
+    imgBottom = new Image();
+    imgBottom.src = '../images/sample1_b.jpg';
 
     imgBottom.addEventListener('load', function() {
-        var ctx;
-        var srcWidth = imgBottom.width,
-            srcHeight = imgBottom.height;
-        var ratio = srcWidth / srcHeight;
-
-        var screenWidth = window.innerWidth;
-        var screenHeight = window.innerHeight;
-
-        var dstWidth;
-        var dstHeight;
-        // if (screenWidth >= screenHeight) {
-        //     dstWidth = screenWidth * imgScale;
-        //     dstHeight = dstWidth / ratio;
-        // } else {
-        //     dstHeight = screenHeight * imgScale;
-        //     dstWidth = dstHeight * ratio;
-        // }
-        dstHeight = screenHeight * imgScale;
-        dstWidth = dstHeight * ratio;
-
-        var container = document.getElementById("container");
-        container.style.width = (dstWidth + 2) + "px";
-
-        var offsetX = canvas.offsetLeft,
-            offsetY = canvas.offsetTop;
-        var mouseDown = false;
-
-        function layer(ctx) {
-            var imgTop = new Image();
-            imgTop.src = '../images/sample1_a.jpg';
-            ctx.fillStyle = 'gray';
-            ctx.drawImage(imgTop, 0, 0, dstWidth, dstHeight);
-        }
-
-        function eventDown(e){
-            e.preventDefault();
-            mouseDown = true;
-        }
-
-        function eventUp(e){
-            e.preventDefault();
-            mouseDown = false;
-        }
-
-        function eventMove(e){
-            e.preventDefault();
-            if(mouseDown) {
-                if(e.changedTouches){
-                    e = e.changedTouches[e.changedTouches.length - 1];
-                }
-                var x = (e.clientX + document.body.scrollLeft || e.pageX) - offsetX || 0,
-                    y = (e.clientY + document.body.scrollTop || e.pageY) - offsetY || 0;
-                ctx.beginPath();
-                ctx.arc(x, y, 10, 0, Math.PI * 2);
-                ctx.fill();
-            }
-        }
-
-        canvas.width = dstWidth;
-        canvas.height = dstHeight;
-        canvas.style.backgroundImage = 'url(../images/sample1_b.jpg)';
-        canvas.style.backgroundSize = dstWidth + "px " + dstHeight + "px";
-        ctx = canvas.getContext('2d');
-        ctx.fillStyle = 'transparent';
-        ctx.fillRect(0, 0, dstWidth, dstHeight);
-        layer(ctx);
-
-        ctx.globalCompositeOperation = 'destination-out';
-
-        canvas.addEventListener('touchstart', eventDown);
-        canvas.addEventListener('touchend', eventUp);
-        canvas.addEventListener('touchmove', eventMove);
-        canvas.addEventListener('mousedown', eventDown);
-        canvas.addEventListener('mouseup', eventUp);
-        canvas.addEventListener('mousemove', eventMove);
+        initCanvas();
     });
+}
+
+function initCanvas() {
+    recalculate();
+
+    var container = document.getElementById("container");
+    container.style.width = (dstWidth + 2) + "px";
+
+    canvas.style.backgroundColor = 'transparent';
+    canvas.style.position = 'absolute';
+
+    canvas.width = dstWidth;
+    canvas.height = dstHeight;
+    canvas.style.backgroundImage = 'url(../images/sample1_b.jpg)';
+    canvas.style.backgroundSize = dstWidth + "px " + dstHeight + "px";
+
+    offsetX = canvas.offsetLeft;
+    offsetY = canvas.offsetTop;
+
+    ctx = canvas.getContext('2d');
+    ctx.fillStyle = 'transparent';
+    ctx.fillRect(0, 0, dstWidth, dstHeight);
+    layer(dstWidth, dstHeight);
+    ctx.globalCompositeOperation = 'destination-out';
+
+    canvas.addEventListener('touchstart', eventDown);
+    canvas.addEventListener('touchend', eventUp);
+    canvas.addEventListener('touchmove', eventMove);
+    canvas.addEventListener('mousedown', eventDown);
+    canvas.addEventListener('mouseup', eventUp);
+    canvas.addEventListener('mousemove', eventMove);
+    canvas.addEventListener('mouseout', eventUp);
+}
+
+function recalculate() {
+    var srcWidth = imgBottom.width,
+        srcHeight = imgBottom.height;
+    var ratio = srcWidth / srcHeight;
+
+    // var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+
+    // if (screenWidth >= screenHeight) {
+    //     dstWidth = screenWidth * imgScale;
+    //     dstHeight = dstWidth / ratio;
+    // } else {
+    //     dstHeight = screenHeight * imgScale;
+    //     dstWidth = dstHeight * ratio;
+    // }
+    dstHeight = screenHeight * imgScale;
+    dstWidth = dstHeight * ratio;
+}
+
+function layer(dstWidth, dstHeight) {
+    ctx.fillStyle = 'gray';
+    ctx.drawImage(imgTop, 0, 0, dstWidth, dstHeight);
+}
+
+function eventDown(e){
+    e.preventDefault();
+    mouseDown = true;
+}
+
+function eventUp(e){
+    e.preventDefault();
+    mouseDown = false;
+}
+
+function eventMove(e){
+    e.preventDefault();
+    if(mouseDown) {
+        if(e.changedTouches){
+            e = e.changedTouches[e.changedTouches.length - 1];
+        }
+        var x = (e.clientX + document.body.scrollLeft || e.pageX) - offsetX || 0,
+            y = (e.clientY + document.body.scrollTop || e.pageY) - offsetY || 0;
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fill();
+        // ctx.stroke();
+    }
 }
