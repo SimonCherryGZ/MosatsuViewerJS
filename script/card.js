ea4d5e3f118e49a5874bb6186f37acff18bac4a8
SimonCherry <simoncherry@sina.com>
2017-06-29 12:28:26 +0800

    fix: 解决微信浏览器reload无效的问题。

--- script/card.js
+++ script/card.js
@@ -141,3 +141,27 @@ function eventMove(e){
         // ctx.stroke();
     }
 }
+
+function updateUrl(url, key) {
+    key = (key || 't') + '=';  //默认是“t”
+    var reg = new RegExp(key + '\\d+');  //正则：t=1472286066028
+    var timestamp = + new Date();
+    if(url.indexOf(key) > -1) {  //有时间戳，直接更新
+        return url.replace(reg, key + timestamp);
+    } else {  //没有时间戳，加上时间戳
+        if(url.indexOf('\?') > -1) {
+            var urlArr = url.split('\?');
+            if(urlArr[1]) {
+                return urlArr[0] + '?' + key + timestamp + '&' + urlArr[1];
+            } else {
+                return urlArr[0] + '?' + key + timestamp;
+            }
+        } else {
+            if(url.indexOf('#') > -1) {
+                return url.split('#')[0] + '?' + key + timestamp + location.hash;
+            } else {
+                return url + '?' + key + timestamp;
+            }
+        }
+    }
+}
